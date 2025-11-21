c50989e3f7c16fb822455b72c03a0356793262d2
Caleb Kavindu Keli <kelicaleb7@gmail.com>
2025-11-21 10:24:06 +0300

    Emails

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
 import express from 'express'; 
 import dotenv from 'dotenv'; 
+import { Sequelize, DataTypes} from 'sequelize'
+
+
+
+const app = express()
+dotenv.config()
+const port = process.env.PORT;
+app.use(express.json())
+
+
+const sequelize = new Sequelize("postgres", process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host:process.env.DB_HOST, 
+    logging: console.log, 
+    dialect: "postgres"
+ })
+
+
+ const emails = sequelize.define('emails', { 
+    emails_id:{ 
+        type: DataTypes.INTEGER, 
+        allowNull:true, 
+        primaryKey:true
+    }, 
+    emails:{ 
+        type: DataTypes.STRING, 
+        allowNull:false
+    }, 
+    messages:{ 
+        type: DataTypes.STRING, 
+        allowNull: false
+    }
+ }, 
+{
+    tableName:"emails", 
+    timestamps:false
+})
+
+emails.sync()
+.then((res) => console.log("Connected to emails successfully", res))
+
+app.get("/", async (req, res) => { 
+    try{ 
+        const findEmails = await emails.findAll()
+        return res.status(200).json(findEmails)
+    }
+    catch(err){ 
+        console.log("error fetching emails", err)
+    }
+})
+
+
+app.listen(port, () => 
+{
+    console.log(`server is running on ${port}`)
+})
+
+
 
 
 
