9b704fe6cbb01397e5a1f4b71b114c52e04a2b6b
kelicaleb <kelicaleb7@gmail.com>
2025-11-22 16:41:45 +0300

    Remove SSL and set port 6543 for pooler

--- index.js
+++ index.js
@@ -18,14 +18,9 @@ app.get("/", async(req, res)=>
     try{
         const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
             host: process.env.DB_HOST,
+            port: 6543,
             dialect: 'postgres',
-            logging: false,
-            dialectOptions: {
-                ssl: {
-                    require: true,
-                    rejectUnauthorized: false
-                }
-            }
+            logging: false
         })
         await sequelize.authenticate()
         const [results] = await sequelize.query('SELECT * FROM emails')
