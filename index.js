b2693337bca362b42acf384ca70dde914908548d
kelicaleb <kelicaleb7@gmail.com>
2025-11-22 17:03:24 +0300

    Change pooler port to 5432

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", async(req, res)=>
     try{
         const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
             host: process.env.DB_HOST,
-            port: 6543,
+            port: 5432,
             dialect: 'postgres',
             logging: false
         })
